e3215037bb674d710086325b8d524e24b80f9706
Lisandro Rossi <Lisandrorossi444@gmail.com>
2021-04-18 01:21:15 -0300

    add Graph and Graph.Auth libraries

--- ApiGraph/Controllers/ValuesController.cs
+++ ApiGraph/Controllers/ValuesController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Microsoft.Graph;
+using Microsoft.Graph.Auth;
+using Microsoft.Identity.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace ApiGraph.Controllers
@@ -13,6 +17,36 @@ namespace ApiGraph.Controllers
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet]
+        [Route("api/Integration/SendMail")]
+        public async Task<IHttpActionResult> SendMail()
+        {
+            try
+            {
+                var clientId = "3d05fbdd-713c-40d7-be36-3b2a7344d860";
+                var tenantId = "629fd4e8-9d26-4da5-85ff-cc01ca1948c4";
+                var clientSecret = "C-vI8s0VlB1TCTY~lq39y1dg5Q~tZ9kxX.";
+
+
+                IConfidentialClientApplication confidentialClientApplication = ConfidentialClientApplicationBuilder
+                    .Create(clientId)
+                    .WithTenantId(tenantId)
+                    .WithClientSecret(clientSecret)
+                    .Build();
+
+                ClientCredentialProvider authProvider = new ClientCredentialProvider(confidentialClientApplication);
+                GraphServiceClient graphClient = new GraphServiceClient(authProvider);
+
+                var groups = await graphClient.Groups.Request().Select(x => new { x.Id, x.DisplayName }).GetAsync();
+
+                return Ok("Success" + groups.FirstOrDefault().DisplayName);
+            }
+            catch (Exception e)
+            {
+                return Ok("Error Message:" + e.Message + "\n" + e.StackTrace);
+            }
         }
 
         // GET api/values/5
